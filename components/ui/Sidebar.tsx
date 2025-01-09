48b797e6d82c1e33c19d0d922e182c662f950f89
Chatanya Pratap <chatanyapra@gmail.com>
2025-01-09 12:31:02 +0530

    sidebar complete

--- components/ui/Sidebar.tsx
+++ components/ui/Sidebar.tsx
@@ -1,13 +1,17 @@
-import { Link } from 'lucide-react'
+"use client"
+import { sidebarLinks } from '@/constants'
+import { cn } from '@/lib/utils'
+import Link from 'next/link';
 import Image from 'next/image'
-import React from 'react'
+import { usePathname } from 'next/navigation'
 
 const Sidebar = ({user}:SiderbarProps) => {
+  const pathname = usePathname();
   return (
     <section className='sidebar'>
       <nav className='flex flex-col gap-4'>
         <Link href='/'
-            className='mb-12 cursor-pointer items-center gap-2'
+            className='mb-12 cursor-pointer flex items-center gap-2'
         >
             <Image
                 src='/icons/logo.svg'
@@ -16,9 +20,32 @@ const Sidebar = ({user}:SiderbarProps) => {
                 height={34}
                 className='size-[24px] max-xl:size-14'
             />
-            <h1  className='sidebar-logo'> </h1>
+            <h1 className='sidebar-logo'>Horizon</h1>
         </Link>
+        {sidebarLinks.map((items) => {
+          const isActive = pathname === items.route || pathname.startsWith(`${items.route}/`);
+          return(
+            <Link href={items.route} key={items.label}
+              className={cn('sidebar-link', {'bg-bank-gradient': isActive})}
+            >
+              <div className='relative size-6'>
+                <Image
+                  src={items.imgURL}
+                  alt={items.label}
+                  fill
+                  className={cn({'brightness-[3] invert-0' : isActive})}                />
+              </div>
+              <p className={cn('sidebar-label', {'!text-white': isActive})}>
+                {items.label}
+              </p>
+            </Link>
+          )  
+        })}
+
+        User
       </nav>
+      
+      Footer
     </section>
   )
 }
