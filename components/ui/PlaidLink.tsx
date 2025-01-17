8329bc200187fdd06b595883ecd2a673688a7d8f
Chatanya Pratap <chatanyapra@gmail.com>
2025-01-17 05:56:02 +0530

    Install packages and setup the react-plaid for bank authentication

---
+++ components/ui/PlaidLink.tsx
@@ -0,0 +1,57 @@
+import React, { useCallback, useEffect, useState } from 'react'
+import { Button } from './button'
+import { useRouter } from 'next/navigation';
+
+interface PlaidLinkOptions {
+    token: string;
+    onSuccess: (public_token: string) => void;
+}
+
+const PlaidLink = ({ user, variant }: PlaidLinkProps) => {
+    const router = useRouter();
+    const [token, setToken] = useState('');
+
+    useEffect(() => {
+        const getLinkToken = async () => {
+            // const data = await createLinkToken(user);  
+            // setToken(data?.linkToken);  
+        }
+
+        getLinkToken();
+    }, []);
+
+    const onSuccess = useCallback(async (public_token: string) => {
+        // await exchangePublicToken({  
+        //     publicToken: public_token,  
+        //     user,  
+        // });  
+
+        router.push('/');
+    }, [user]);
+
+    const config: PlaidLinkOptions = {
+        token,
+        onSuccess,
+    };
+
+    const {open, ready} = usePlaidLink(config);
+    return (
+        <>
+            {variant === 'primary' ? (
+                <Button className='plaidlink-primary'>
+                    Connect bank
+                </Button>
+            ) : variant === 'ghost' ? (
+                <Button>
+                    Connect bank
+                </Button>
+            ) : (
+                <Button>
+                    Connect bank
+                </Button>
+            )}
+        </>
+    )
+}
+
+export default PlaidLink
